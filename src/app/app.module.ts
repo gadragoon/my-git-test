19c568de85a0e97970e9e1d8e669ab7fd91181c3
gadragoon <gdisnotgd@gmail.com>
2021-01-17 18:10:52 +0900

    create new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewComComponent } from './new-com/new-com.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewComComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/new-com/new-com.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NewComComponent } from './new-com.component';
+
+describe('NewComComponent', () => {
+  let component: NewComComponent;
+  let fixture: ComponentFixture<NewComComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NewComComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NewComComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
